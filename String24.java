4c27a193fd58ca22dbd28f3b8facd1cefeb37156
Kaarthi <kaarthisenthilkumar@gmail.com>
2026-04-01 10:00:00 +0530

    Added code for April 1

---
+++ String24.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class String24 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s1 = scan.next();
+        System.out.println(s1);
+        scan.close();
+    }
+}
