4fe8fdff6816c67acbc73548ade6fc6037142453
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-07-23 13:54:50 +0530

    Append empty string to output in main method

--- String24.java
+++ String24.java
@@ -4,7 +4,7 @@ public class String24 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s1 = scan.next();
-        System.out.println(s1);
+        System.out.println(s1+"");
         scan.close();
     }
 }
