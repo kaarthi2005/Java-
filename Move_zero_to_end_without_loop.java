05a113dc91ce123f7b1e15d11185ccb8b0f897a2
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-08-02 11:47:17 +0530

    Rename class to Move_zero_to_end__without_loop

--- Move_zero_to_end_without_loop.java
+++ Move_zero_to_end_without_loop.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Move_zero_to_end_without_loop {
+public class Move_zero_to_end__without_loop {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
