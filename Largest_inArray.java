f401fc4f253e5d9df960c4ce0cae803078f8fffa
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-08-30 23:32:16 +0530

    Add files via upload

---
+++ Largest_inArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Largest_inArray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int arr [] = new int [n];
+        for(int i=0 ;i<n ;i++){
+            arr[i] = scan.nextInt();
+        }
+        int max = arr[0];
+        for(int i=0 ;i<n ;i++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+        scan.close();
+    }
+}
